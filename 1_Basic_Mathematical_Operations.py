edc31da3f0e5258a70843f306bda98c0aa34d232
VP1323 <vlovep1323@gmail.com>
2025-04-27 23:08:55 +0530

    Assignment 1

---
+++ 1_Basic_Mathematical_Operations.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+print("\nAddition: ", a+b)
+print("Substraction: ", a-b)
+print("Multiplication: ",a*b)
+print("Division: ", float(a/b))
